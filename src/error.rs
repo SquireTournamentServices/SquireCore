44ff2500c619c517aad238cb2034823cc1f76f4f
TylerBloom <tylerbloom2222@gmail.com>
2022-02-21 12:19:22 -0500

    Added basic error impl for TournamentError... will be improved.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum TournamentError {
     IncorrectStatus,
@@ -10,3 +12,22 @@ pub enum TournamentError {
     InvalidBye,
     InvalidGame,
 }
+
+impl fmt::Display for TournamentError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match &self {
+            Self::IncorrectStatus => "IncorrectStatus",
+            Self::PlayerLookup => "PlayerLookup",
+            Self::RoundLookup => "RoundLookup",
+            Self::DeckLookup => "DeckLookup",
+            Self::RegClosed => "RegClosed",
+            Self::PlayerNotInRound => "PlayerNotInRound",
+            Self::NoActiveRound => "NoActiveRound",
+            Self::InvalidBye => "InvalidBye",
+            Self::InvalidGame => "InvalidGame",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl std::error::Error for TournamentError { }
