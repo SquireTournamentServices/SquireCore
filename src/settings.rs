1f2979c0680977b1683d45df89c06f20b0dc64bc
TylerBloom <tylerbloom2222@gmail.com>
2022-06-11 17:08:46 -0400

    Impl-ed Display for tournament settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -58,3 +60,99 @@ pub enum StandardScoringSetting {
     IncludeOppMwp(bool),
     IncludeOppGwp(bool),
 }
+
+impl fmt::Display for TournamentSetting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use TournamentSetting::*;
+        match self {
+            Format(s) => {
+                write!(f, "Format: {s}")
+            }
+            StartingTableNumber(s) => {
+                write!(f, "First Table#: {s}")
+            }
+            UseTableNumbers(s) => {
+                write!(f, "Table#?: {s}")
+            }
+            MinDeckCount(s) => {
+                write!(f, "Min Deck Count: {s}")
+            }
+            MaxDeckCount(s) => {
+                write!(f, "Min Deck Count: {s}")
+            }
+            RequireCheckIn(s) => {
+                write!(f, "Check Ins?: {}", if *s { "yes" } else { "no" })
+            }
+            RequireDeckReg(s) => {
+                write!(f, "Deck Reg?: {}", if *s { "yes" } else { "no" })
+            }
+            PairingSetting(s) => {
+                write!(f, "{s}")
+            }
+            ScoringSetting(s) => {
+                write!(f, "{s}")
+            }
+        }
+    }
+}
+
+impl fmt::Display for PairingSetting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use PairingSetting::*;
+        match self {
+            Swiss(s) => write!(f, "{s}"),
+            Fluid(s) => write!(f, "{s}"),
+        }
+    }
+}
+
+impl fmt::Display for ScoringSetting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ScoringSetting::*;
+        match self {
+            Standard(s) => write!(f, "{s}"),
+        }
+    }
+}
+
+impl fmt::Display for SwissPairingsSetting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use SwissPairingsSetting::*;
+        match self {
+            MatchSize(s) => write!(f, "Match Size: {s}"),
+            DoCheckIns(s) => write!(f, "Check Ins?: {s}"),
+        }
+    }
+}
+
+impl fmt::Display for FluidPairingsSetting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use FluidPairingsSetting::*;
+        match self {
+            MatchSize(s) => write!(f, "Match Size: {s}"),
+        }
+    }
+}
+
+impl fmt::Display for StandardScoringSetting {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use StandardScoringSetting::*;
+        match self {
+            MatchWinPoints(s) => write!(f, "Match Win: {s}"),
+            MatchDrawPoints(s) => write!(f, "Match Draw: {s}"),
+            MatchLossPoints(s) => write!(f, "Match Loss: {s}"),
+            GameWinPoints(s) => write!(f, "Game Win: {s}"),
+            GameDrawPoints(s) => write!(f, "Game Draw: {s}"),
+            GameLossPoints(s) => write!(f, "Game Loss: {s}"),
+            ByePoints(s) => write!(f, "Bye Win: {s}"),
+            IncludeByes(s) => write!(f, "Byes?: {}", if *s { "yes" } else { "no" }),
+            IncludeMatchPoints(s) => write!(f, "Match Points?: {}", if *s { "yes" } else { "no" }),
+            IncludeGamePoints(s) => write!(f, "Game Points?: {}", if *s { "yes" } else { "no" }),
+            IncludeMwp(s) => write!(f, "MWP?: {}", if *s { "yes" } else { "no" }),
+            IncludeGwp(s) => write!(f, "GWP?: {}", if *s { "yes" } else { "no" }),
+            IncludeOppMwp(s) => write!(f, "Opp MWP?: {}", if *s { "yes" } else { "no" }),
+            IncludeOppGwp(s) => write!(f, "Opp GWP?: {}", if *s { "yes" } else { "no" }),
+        }
+    }
+}
+
