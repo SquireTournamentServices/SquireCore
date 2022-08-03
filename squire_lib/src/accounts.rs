b71a5a36473c3022791af2c5dada68f748f0fb21
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-08-03 17:36:31 -0400

    created a struct for Uuid for type safety

--- squire_lib/src/accounts.rs
+++ squire_lib/src/accounts.rs
@@ -7,17 +7,21 @@ enum SharingPermissions {
     user_name(String),
 }
 
+struct UserId {
+    pub Uuid,
+}
+
 struct SquireAccount {
     display_name: String,
     user_name: String,
-    user_id: Uuid,
+    user_id: UserId,
     do_share: SharingPermissions,
 }
 
 struct OrganizationAccount {
     display_name: String,
     user_name: String,
-    user_id: Uuid,
+    user_id: UserId,
     owner: SquireAccount,
     default_judge: Vec<SquireAccount>,
     admin_account: Vec<SquireAccount>,
