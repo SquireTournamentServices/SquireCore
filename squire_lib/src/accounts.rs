f890cc4581b0c808e9af26ea1b688bceba5132de
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-08-13 00:43:12 -0400

    updated gamer tags storage mechanism, and started to implement CRUD API system. currently implemented CRUD for gamer tags

--- squire_lib/src/accounts.rs
+++ squire_lib/src/accounts.rs
@@ -10,10 +10,17 @@ pub enum SharingPermissions {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub enum Platforms {
+    Cockatrice,
+    MTGOnline,
+    Arena
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct SquireAccount {
     display_name: String,
     user_name: String,
-    gamer_tags = Vec<Option<String>>,
+    gamer_tags: HashMap<Platforms, Option<String>>,
     user_id: UserAccountID,
     do_share: SharingPermissions,
 }
@@ -27,4 +34,33 @@ pub struct OrganizationAccount {
     default_judge: Vec<SquireAccount>,
     admin_account: Vec<SquireAccount>,
     default_tournament_settings: settings,
+}
+
+impl SquireAccount {
+    pub fn create(user_name: String, display_name: String, permissions: SharingPermissions) -> Self {
+        SquireAccount {
+            display_name,
+            user_name,
+            gamer_tags: HashMap::new(),
+            user_id: UserAccountID::new(Uuid::new_v4()),
+            permissions
+        }
+    }
+
+    pub fn update_tags(&mut self, platfrom: Platforms, user_name: String) {
+        self.gamer_tags.insert(platfrom, user_name);
+    }
+
+    pub fn get_tags(&self, platforms: Vec<Platforms>) -> Vec<Option<String>> {
+        let tags = Vec::new()
+        for platform in platforms {
+            let gamer_tag = self.gamer_tags.get(platform);
+            tags.insert(Some(platform.clone()));
+        }
+        tags
+    }
+
+    pub fn delete_tags(&mut self, platform: Platforms) {
+        self.gamer_tags.insert(platform, None)
+    }
 }
