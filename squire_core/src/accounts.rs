886fd78fb231aa94afd7e7d3150bb37210e7adbc
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-09-11 20:45:38 -0400

    added user update methods

--- squire_core/src/accounts.rs
+++ squire_core/src/accounts.rs
@@ -6,7 +6,8 @@ use squire_lib::identifiers::{AdminId, UserAccountId, OrganizationAccountId};
 use squire_lib::accounts::{OrganizationAccount, UserAccount};
 
 use squire_sdk::accounts::{
-    GetAllUsersResponse, GetOrgResponse, GetUserResponse,
+    GetAllUsersResponse, GetOrgResponse, GetUserResponse, GetUserPermissionsResponse, 
+    UpdateSquireAccountResponse,
 };
 
 pub static USERS_MAP: OnceCell<DashMap<UserAccountId, UserAccount>> = OnceCell::new();
@@ -41,7 +42,49 @@ pub fn user_permissions(id: UserAccountId) -> GetUserPermissionsResponse {
         .get()
         .unwrap()
         .get(&UserAccountId(id))
-        .map(|permissions| permissions.get_current_permissions()),
+        .map(|user| user.get_current_permissions()),
+    )
+}
+
+#[post("/users/update/<id>")]
+pub fn update_user_account(id: UserAccountId, 
+    user_name: Option<String>, 
+    display_name: Option<String>,
+    delete_user_name: Option<Boolean>,
+    delete_display_name: Option<Boolean>
+) -> UpdateSquireAccountResponse {
+    UpdateSquireAccountResponse::new(
+        if(user_name.unwrap() != None) {
+            ORGS_MAP
+            .get()
+            .unwrap()
+            .get(&UserAccountId(id))
+            .map(|user| user.change_user_name(user_name)),
+        }
+
+        if(display_name.unwrap() != None) {
+            ORGS_MAP
+            .get()
+            .unwrap()
+            .get(&UserAccountId(id))
+            .map(|user| user.change_display_name(display_name)),
+        }
+
+        if(delete_user_name.unwrap()) {
+            ORGS_MAP
+            .get()
+            .unwrap()
+            .get(&UserAccountId(id))
+            .map(|user| user.delete_user_name()),
+        }
+
+        if(delete_display_name.unwrap()) {
+            ORGS_MAP
+            .get()
+            .unwrap()
+            .get(&UserAccountId(id))
+            .map(|user| user.delete_display_name()),
+        }
     )
 }
 
--- squire_sdk/src/accounts.rs
+++ squire_sdk/src/accounts.rs
@@ -14,3 +14,12 @@ pub type GetUserResponse = SquireResponse<Option<UserAccount>>;
 pub type GetUserPermissionsResponse = SquireResponse<Option<UserAccount::permissions>>;
 
 pub type GetOrgResponse = SquireResponse<Option<OrganizationAccount>>;
+
+// In SDK, to be used at the body of a POST request
+pub struct UpdateSquireAccountRequest {
+    display_name: Option<String>,
+    
+    user_name: Option<String>,
+  }
+
+pub type UpdateSquireAccountResponse = SquireResponse<UpdateSquireAccountRequest>;
