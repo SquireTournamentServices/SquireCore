e29e6c5e877c85b34022d96d2a2152f9cdab47b0
Danny Piper <djpiper28@gmail.com>
2022-12-26 22:30:02 +0000

    fix: ffi bindings now return generated HTML

--- squire_lib/src/ffi/ffi_tournament.rs
+++ squire_lib/src/ffi/ffi_tournament.rs
@@ -641,11 +641,13 @@ impl TournamentId {
     /// NULL on error
     #[no_mangle]
     pub extern "C" fn tid_round_slips_html(self, __css: *const c_char) -> *const c_char {
-        let mut ret: String = Default::default();
+        let mut ret: *const c_char = std::ptr::null();
         SQUIRE_RUNTIME.get().unwrap().tournament_query(self, |t| {
-            clone_string_to_c_string(&t.round_slips_html(unsafe { CStr::from_ptr(__css).to_str().unwrap() })
-        )});
-        clone_string_to_c_string(ret.as_str())
+            ret = clone_string_to_c_string(
+                &t.round_slips_html(unsafe { CStr::from_ptr(__css).to_str().unwrap() }),
+            );
+        });
+        return ret;
     }
 
     // End of getters
