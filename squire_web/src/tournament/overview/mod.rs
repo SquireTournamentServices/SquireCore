f8e7661b9edcf8685199c9c2ec5d814e6b669eca
wrobins6 <43558055+wrobins6@users.noreply.github.com>
2023-03-23 17:59:32 -0400

    Updated tournament overview display
    Improved tournament overview visuals. Also added logic to retrieve and display active rounds, certified rounds, registered players, and dropped players.

--- squire_web/src/tournament/overview/mod.rs
+++ squire_web/src/tournament/overview/mod.rs
@@ -27,16 +27,32 @@ impl Component for TournOverview {
             .unwrap()
             .state
             .query_tournament(&self.id, |t| {
+                let activeRounds = t.round_reg.rounds.values().filter(|r| r.is_active()).count();
+                let certifiedRounds = t.round_reg.rounds.values().filter(|r| r.is_certified()).count();
+                let registeredPlayers = t.player_reg.players.values().filter(|p| p.can_play()).count();
+                let droppedPlayers = t.player_reg.players.values().filter(|p| !p.can_play()).count();
+
                 html! {
-                    <>
-                    <h1 align="center">{ format!("Welcome to {}", t.name) }</h1>
-                    <p align="center">{ format!("Format : {}", t.format) }</p>
-                    <p align="center">{ format!("Status : {}", t.status) }</p>
-                    <p align="center">{ format!("Number of players : {}", t.player_reg.players.len()) }</p>
-                    <p align="center">{ format!("Number of rounds : {}", t.round_reg.rounds.len()) }</p>
-                    <p align="center">{ format!("Number of judges : {}", t.judges.len()) }</p>
-                    <p align="center">{ format!("Number of admins : {}", t.admins.len()) }</p>
-                    </>
+                    <div class="m-lg-0 m-md-4 m-0">
+                        <div class="p-5 bg-light rounded-3">
+                            <div class="container-fluid p-md-5">
+                                <h1 class="display-5 fw-bold">{ format!("Welcome to {}", t.name) }</h1>
+                                <hr class="my-4"/>
+                                <p>{ format!("Format : {}", t.format) }</p>
+                                <p>{ format!("Status : {}", t.status) }</p>
+                                <p>{ format!("Registered players : {}", registeredPlayers) }</p>
+
+                                if droppedPlayers > 0 {
+                                    <p>{ format!("Dropped players : {}", droppedPlayers) }</p>
+                                }
+
+                                <p>{ format!("Active rounds : {}", activeRounds) }</p>
+                                <p>{ format!("Certified rounds : {}", certifiedRounds) }</p>
+                                <p>{ format!("Number of judges : {}", t.judges.len()) }</p>
+                                <p>{ format!("Number of admins : {}", t.admins.len()) }</p>
+                            </div>
+                        </div>
+                    </div>
                 }
             })
             .unwrap_or_default()
