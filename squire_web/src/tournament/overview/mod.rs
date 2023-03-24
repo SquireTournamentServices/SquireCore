c87c8a8ab7a6943c9b5541bc44ac6de1b3b2517f
wrobins6 <43558055+wrobins6@users.noreply.github.com>
2023-03-23 22:43:57 -0400

    Implement suggested changes into tournament overview

--- squire_web/src/tournament/overview/mod.rs
+++ squire_web/src/tournament/overview/mod.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 
 use squire_sdk::{client::state::ClientState, tournaments::TournamentId};
+use squire_sdk::model::{players::PlayerStatus, rounds::RoundStatus};
 
 use crate::CLIENT;
 
@@ -27,27 +28,40 @@ impl Component for TournOverview {
             .unwrap()
             .state
             .query_tournament(&self.id, |t| {
-                let activeRounds = t.round_reg.rounds.values().filter(|r| r.is_active()).count();
-                let certifiedRounds = t.round_reg.rounds.values().filter(|r| r.is_certified()).count();
-                let registeredPlayers = t.player_reg.players.values().filter(|p| p.can_play()).count();
-                let droppedPlayers = t.player_reg.players.values().filter(|p| !p.can_play()).count();
+                let (activeRnds, certifiedRnds) = t.round_reg.rounds.values().fold((0, 0), |mut acc, r| {
+                    match r.status {
+                        RoundStatus::Open => acc.0 += 1,
+                        RoundStatus::Certified => acc.1 += 1,
+                        _ => {}
+                    }
+                    acc
+                });
+
+                let (registeredPlyrs, droppedPlyrs) = t.player_reg.players.values().fold((0, 0), |mut acc, p| {
+                    match p.status {
+                        PlayerStatus::Registered => acc.0 += 1,
+                        PlayerStatus::Dropped => acc.1 += 1,
+                        _ => {}
+                    }
+                    acc
+                });
 
                 html! {
-                    <div class="m-lg-0 m-md-4 m-0">
+                    <div class="m-lg-0 m-md-4 my-3">
                         <div class="p-5 bg-light rounded-3">
                             <div class="container-fluid p-md-5">
                                 <h1 class="display-5 fw-bold">{ format!("Welcome to {}", t.name) }</h1>
                                 <hr class="my-4"/>
                                 <p>{ format!("Format : {}", t.format) }</p>
                                 <p>{ format!("Status : {}", t.status) }</p>
-                                <p>{ format!("Registered players : {}", registeredPlayers) }</p>
+                                <p>{ format!("Registered players : {registeredPlyrs}") }</p>
 
-                                if droppedPlayers > 0 {
-                                    <p>{ format!("Dropped players : {}", droppedPlayers) }</p>
+                                if droppedPlyrs > 0 {
+                                    <p>{ format!("Dropped players : {droppedPlyrs}") }</p>
                                 }
 
-                                <p>{ format!("Active rounds : {}", activeRounds) }</p>
-                                <p>{ format!("Certified rounds : {}", certifiedRounds) }</p>
+                                <p>{ format!("Active rounds : {activeRnds}") }</p>
+                                <p>{ format!("Certified rounds : {certifiedRnds}") }</p>
                                 <p>{ format!("Number of judges : {}", t.judges.len()) }</p>
                                 <p>{ format!("Number of admins : {}", t.admins.len()) }</p>
                             </div>
