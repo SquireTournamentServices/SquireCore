a50581ac31a464f1914f01425e0503a038bf1bbd
TylerBloom <tylerbloom2222@gmail.com>
2023-04-07 00:03:38 -0400

    Added functional round timer to selected round/round view

--- squire_web/src/tournament/rounds/selected.rs
+++ squire_web/src/tournament/rounds/selected.rs
@@ -1,3 +1,4 @@
+use chrono::{Duration, Utc};
 use squire_sdk::model::{
     rounds::{Round, RoundId, RoundStatus},
     tournament::Tournament,
@@ -24,72 +25,83 @@ pub fn round_info_display(rnd: &Round) -> Html {
 }
 
 pub struct SelectedRound {
-    id: Option<RoundId>,
+    pub(crate) id: RoundId,
 }
 
 impl SelectedRound {
-    pub fn new() -> Self {
-        Self { id: None }
+    pub fn new(id: RoundId) -> Self {
+        Self { id }
     }
 
-    pub fn update(&mut self, id: Option<RoundId>) -> bool {
+    pub fn update(&mut self, id: RoundId) -> bool {
         let digest = self.id != id;
         self.id = id;
         digest
     }
 
     pub fn view(&self, tourn: &Tournament) -> Html {
-        let returnhtml = self
-            .id
-            .map(|id| {
-                tourn
-                    .get_round(&id.into())
-                    .map(|rnd| {
-                        html! {
-                            <>
-                            <>{round_info_display(rnd)}</>
-                            <ul>
-                            {
-                                rnd.players.clone().into_iter()
-                                    // Right now this code is duplicated, however once SelectedRound has more functionality it will be made significantly different. (It will have onclick functionality.)
-                                    .map(|pid| {
-                                        let player_in_round = { ||
-                                            tourn
-                                            .get_player(&pid.into())
-                                            .map(|p| p.name.as_str())
-                                            .unwrap_or_else( |_| "Player not found")
-                                        };
-                                        let player_wins = rnd.results.get(&pid.into()).unwrap_or(&0);
-                                        let player_confirm = rnd.confirmations.get(&pid.into()).is_some();
-                                        html! { 
-                                            <li>
-                                            <div>
-                                            { format!( "{}", player_in_round()) }
-                                            </div>
-                                            <div>
-                                            { format!( "wins : {}, confirmed : {}", player_wins, player_confirm ) }
-                                            </div>
-                                            </li>
-                                        }
-                                    })
-                                    .collect::<Html>()
-                            }
-                            </ul>
-                            <p>
-                            { format!("Draws : {}", rnd.draws) }
-                            </p>
-                            </>
-                        }
-                    })
-                    .unwrap_or_else(|_| html!{
-                        <h4>{"Round not found"}</h4>
-                    })
+        let returnhtml = tourn
+            .get_round(&self.id.into())
+            .map(|rnd| {
+                // TODO: Remove unwrap here
+                let dur_left = Duration::from_std(rnd.length + rnd.extension).unwrap() - (Utc::now() - rnd.timer);
+                html! {
+                    <>
+                    <>{round_info_display(rnd)}</>
+                    <ul>
+                    {
+                        rnd.players.clone().into_iter()
+                            // Right now this code is duplicated, however once SelectedRound has more functionality it will be made significantly different. (It will have onclick functionality.)
+                            .map(|pid| {
+                                let player_in_round = { ||
+                                    tourn
+                                    .get_player(&pid.into())
+                                    .map(|p| p.name.as_str())
+                                    .unwrap_or_else( |_| "Player not found")
+                                };
+                                let player_wins = rnd.results.get(&pid.into()).unwrap_or(&0);
+                                let player_confirm = rnd.confirmations.get(&pid.into()).is_some();
+                                html! { 
+                                    <li>
+                                    <div>
+                                    { format!( "{}", player_in_round()) }
+                                    </div>
+                                    <div>
+                                    { format!( "wins : {}, confirmed : {}", player_wins, player_confirm ) }
+                                    </div>
+                                    </li>
+                                }
+                            })
+                            .collect::<Html>()
+                    }
+                    </ul>
+                    <p>
+                    { format!("Draws : {}", rnd.draws) }
+                    </p>
+                    <p>
+                    { 
+                        pretty_print_duration(dur_left)
+                    }
+                    </p>
+                    </>
+                }
             })
-            .unwrap_or_else(|| html!{
-                <h4>{"No round selected"}</h4>
+            .unwrap_or_else(|_| html!{
+                <h4>{"Round not found"}</h4>
             });
         return html!{
             <div class="m-2">{returnhtml}</div>
         };
+    }
+}
+
+fn pretty_print_duration(dur: Duration) -> String {
+    let hours = dur.num_hours();
+    let mins = dur.num_minutes();
+    let secs = dur.num_seconds();
+    if hours < 0 {
+        format!("Time left: {hours}:{}:{}", mins.abs() % 60, secs.abs() % 60)
+    } else {
+        format!("Over time: {}:{}:{}", hours.abs(), mins.abs() % 60, secs.abs() % 60)
     }
 }
