82d1adeec609d3ff5127c62b6f373a1e160b6b5c
TylerBloom <tylerbloom2222@gmail.com>
2023-04-21 15:30:57 -0400

    Refactoring SquireClient to be universal, to ClientState needed

--- squire_sdk/src/client/error.rs
+++ squire_sdk/src/client/error.rs
@@ -1,10 +1,30 @@
 use reqwest::StatusCode;
+use squire_lib::error::TournamentError;
 
 #[derive(Debug)]
 pub enum ClientError {
-    Reqwest(reqwest::Error),
-    RequestStatus(StatusCode),
     NotLoggedIn,
     LogInFailed,
     FailedToConnect,
+    Reqwest(reqwest::Error),
+    RequestStatus(StatusCode),
+    Tournament(TournamentError)
+}
+
+impl From<StatusCode> for ClientError {
+    fn from(status: StatusCode) -> Self {
+        Self::RequestStatus(status)
+    }
+}
+
+impl From<reqwest::Error> for ClientError {
+    fn from(errro: reqwest::Error) -> Self {
+        Self::Reqwest(error)
+    }
+}
+
+impl From<TournamentError> for ClientError {
+    fn from(error: TournamentError) -> Self {
+        Self::Tournament(error)
+    }
 }
