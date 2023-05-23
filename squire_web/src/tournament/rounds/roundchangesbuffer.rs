44caa6003b54e6efae67125b6b0592985f88b08f
craigslisttrashman <fintanneff@gmail.com>
2023-05-23 18:16:09 -0400

    Time extender implemented.
    Time extender implemented.

--- squire_web/src/tournament/rounds/roundchangesbuffer.rs
+++ squire_web/src/tournament/rounds/roundchangesbuffer.rs
@@ -5,7 +5,7 @@ use squire_sdk::{
 use std::{collections::HashMap, fmt::Display, marker::PhantomData, rc::Rc, str::FromStr};
 use yew::prelude::*;
 
-use super::{RoundResultTicker, RoundResultTickerMessage, RoundConfirmationTicker, RoundConfirmationTickerMessage};
+use super::{RoundResultTicker, RoundResultTickerMessage, RoundConfirmationTicker, RoundConfirmationTickerMessage, SelectedRoundMessage};
 
 #[derive(Debug, PartialEq, Clone)]
 /// Data buffer holding changes which can be pushed to a round using admin operations
@@ -13,7 +13,9 @@ pub struct RoundChangesBuffer {
     pub rid: RoundId,
     pub draw_ticker: RoundResultTicker,
     pub win_tickers: HashMap<PlayerId, RoundResultTicker>,
-    pub confirmation_tickers: HashMap<PlayerId, RoundConfirmationTicker>
+    pub confirmation_tickers: HashMap<PlayerId, RoundConfirmationTicker>,
+    pub current_extension_minutes: u64,
+    pub process: Callback<SelectedRoundMessage>,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -21,16 +23,20 @@ pub struct RoundChangesBuffer {
 pub enum RoundChangesBufferMessage {
     TickClicked(Option<PlayerId>, RoundResultTickerMessage),
     ConfirmationClicked(PlayerId, RoundConfirmationTickerMessage),
+    ExtensionIncrease(),
+    ExtensionDecrease(),
     ResetAll(),
 }
 
 impl RoundChangesBuffer {
-    pub fn new(rid: RoundId, draw_ticker: RoundResultTicker) -> Self {
+    pub fn new(process: Callback<SelectedRoundMessage>, rid: RoundId, draw_ticker: RoundResultTicker) -> Self {
         Self {
             rid,
             draw_ticker,
             win_tickers: HashMap::new(),
             confirmation_tickers: HashMap::new(),
+            current_extension_minutes: 0,
+            process
         }
     }
 
@@ -45,6 +51,12 @@ impl RoundChangesBuffer {
             }
             RoundChangesBufferMessage::ConfirmationClicked(pid, rctmsg) => {
                 self.confirmation_tickers.get_mut(&pid).unwrap().update(rctmsg);
+            }
+            RoundChangesBufferMessage::ExtensionIncrease() => {
+                self.current_extension_minutes += 1;
+            }
+            RoundChangesBufferMessage::ExtensionDecrease() => {
+                self.current_extension_minutes = self.current_extension_minutes.saturating_sub(1);
             }
             RoundChangesBufferMessage::ResetAll() => {
                 self.draw_ticker
@@ -71,4 +83,28 @@ impl RoundChangesBuffer {
     pub fn view_draw_ticker(&self) -> Html {
         self.draw_ticker.view()
     }
+
+    /// View extension ticker
+    pub fn view_extension_ticker(&self) -> Html {
+        let cb = self.process.clone();
+        let up = move |s| {
+            cb.emit(SelectedRoundMessage::BufferMessage(
+                RoundChangesBufferMessage::ExtensionIncrease()
+            ));
+        };
+        let cb = self.process.clone();
+        let down = move |s| {
+            cb.emit(SelectedRoundMessage::BufferMessage(
+                RoundChangesBufferMessage::ExtensionDecrease()
+            ));
+        };
+        html! {
+            <>
+            <>{ format!("Time to extend in minutes : {}", self.current_extension_minutes.to_string()) }</>
+            <button onclick={up}>{"+"}</button>
+            <button onclick={down}>{"-"}</button>
+            </>
+        }
+    }
+
 }
