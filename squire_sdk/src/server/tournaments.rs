965320d4ca454cc739c9dd6229e093f26ed7251a
TylerBloom <tylerbloom2222@gmail.com>
2023-06-01 18:08:01 -0600

    Implemented basic Websocket handler in SquireClient.

--- squire_sdk/src/server/tournaments.rs
+++ squire_sdk/src/server/tournaments.rs
@@ -9,10 +9,7 @@ use axum::{
 };
 
 use crate::{
-    api::{
-        CREATE_TOURNAMENT_ENDPOINT, GET_ALL_ACTIVE_TOURNAMENTS_ENDPOINT, GET_TOURNAMENT_ENDPOINT,
-        ROLLBACK_TOURNAMENT_ENDPOINT, SYNC_TOURNAMENT_ENDPOINT,
-    },
+    api::GET_TOURNAMENT_ENDPOINT,
     server::{state::ServerState, User},
     tournaments::*,
     utils::Url,
@@ -22,12 +19,7 @@ pub fn get_routes<S>() -> Router<S>
 where
     S: ServerState,
 {
-    Router::new()
-        .route(GET_TOURNAMENT_ENDPOINT.as_str(), get(get_tournament::<S>))
-        .route(
-            GET_ALL_ACTIVE_TOURNAMENTS_ENDPOINT.as_str(),
-            get(get_all_tournaments::<S>),
-        )
+    Router::new().route(GET_TOURNAMENT_ENDPOINT.as_str(), get(get_tournament::<S>))
 }
 
 pub async fn get_tournament<S>(
@@ -40,14 +32,7 @@ where
     GetTournamentResponse::new(state.query_tournament(&id, |t| t.clone()).await)
 }
 
-pub async fn get_all_tournaments<S>(State(state): State<S>) -> GetAllTournamentsResponse
-where
-    S: ServerState,
-{
-    GetAllTournamentsResponse::new(state.query_all_tournaments(|t| t.clone()).await)
-}
-
-/// Adds a user to the gathering via a subsocket
+/// Adds a user to the gathering via a websocket
 pub async fn join_gathering() {
     todo!()
 }
--- squire_sdk/src/sync/messages.rs
+++ squire_sdk/src/sync/messages.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
+use squire_lib::tournament::TournamentId;
 use ulid::Ulid;
 
-use super::OpSync;
+use super::{OpSync, TournamentManager};
 
 pub type ServerBoundMessage = WebSocketMessage<ServerBound>;
 pub type ClientBoundMessage = WebSocketMessage<ServerBound>;
@@ -14,8 +15,19 @@ pub struct WebSocketMessage<B> {
     pub body: B
 }
 
+impl<B> WebSocketMessage<B> {
+    pub fn new(body: B) -> Self {
+        Self {
+            id: Ulid::new(),
+            body,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum ServerBound {
+    /// Asks the server to send back a copy of the tournament manager for the tournament
+    Fetch(TournamentId),
     /// New operations have been sent from a client and need to be merged
     SyncReq(OpSync),
     /// A sync request was forwarded to clients. This communicates that the client received and
@@ -26,6 +38,8 @@ pub enum ServerBound {
 #[allow(clippy::large_enum_variant)]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum ClientBound {
+    /// Contains a copy of the tournament manager (sent in response to `ServerBound::Fetch`)
+    FetchResp(TournamentManager),
     /// The result of trying to process the sync request
     SyncResp(()),
     /// The server has successfully synced with a client. This forwards those changes
