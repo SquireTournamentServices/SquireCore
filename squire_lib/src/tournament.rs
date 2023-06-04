8a933bf0b8a281556116dd5e0ceb688d5a8196f2
zbrachinara <zbrachinara@gmail.com>
2023-06-03 22:38:53 -0400

    use the correct error in `give_time_extension`

--- squire_lib/src/tournament.rs
+++ squire_lib/src/tournament.rs
@@ -386,7 +386,7 @@ impl Tournament {
         if let Some(new_extension) = round.extension.checked_add(ext) {
             round.extension = new_extension;
         } else {
-            return Err(TournamentError::ActiveMatches)
+            return Err(TournamentError::TimeOverflow)
         }
         Ok(OpData::Nothing)
     }
