77972c1300ca72ec721285d5569f3571e50d7f48
zbrachinara <zbrachinara@gmail.com>
2023-06-08 22:13:03 -0400

    new implementation for `greedy_pairings`
    take iterator instead of slice in `valid_pairing`
    
    correct removal order in `greedy`
    
    IntoIter for player parameter on greedy pairing
    
    remove an allocation
    
    simplify extend statement
    
    remove tap dependency
    
    pop first player from correct side
    
    collapse temporary pairing vec
    
    correct pairing success condition
    
    write conditional only once

--- squire_lib/src/pairings/greedy.rs
+++ squire_lib/src/pairings/greedy.rs
@@ -1,58 +1,54 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::{identifiers::PlayerId, pairings::Pairings};
 
 // TODO: PLEASE provide a better description
 /// A simple, greedy algorithm that attempts to pair the first players first
-pub fn greedy_pairings(
-    mut plyrs: Vec<PlayerId>,
+pub fn greedy_pairings<Players>(
+    plyrs: Players,
     opps: &HashMap<PlayerId, HashSet<PlayerId>>,
     match_size: usize,
     repair_tol: u64,
-) -> Pairings {
+) -> Pairings
+where
+    Players: IntoIterator<Item = PlayerId>,
+{
+    let mut plyrs: VecDeque<_> = plyrs.into_iter().collect();
     let mut digest = Pairings {
         paired: Vec::with_capacity(plyrs.len() / match_size + 1),
         rejected: Vec::new(),
     };
-    while plyrs.len() >= match_size {
-        let mut index_buffer: Vec<usize> = Vec::with_capacity(match_size);
+    'outer: while plyrs.len() >= match_size {
+        let Some(first) = plyrs.pop_front() else { break; };
         let mut id_buffer: Vec<PlayerId> = Vec::with_capacity(match_size);
-        index_buffer.push(0);
-        id_buffer.push(plyrs[0]);
-        for (i, _) in plyrs.iter().enumerate().skip(1) {
-            if valid_pairing(opps, &id_buffer, &plyrs[i], repair_tol) {
-                index_buffer.push(i);
-                id_buffer.push(plyrs[i]);
-                if index_buffer.len() == match_size {
-                    break;
+
+        for plyr in &plyrs {
+            let current_pairing = std::iter::once(&first).chain(id_buffer.iter());
+            if valid_pairing(opps, current_pairing, plyr, repair_tol) {
+                id_buffer.push(*plyr);
+                if id_buffer.len() == match_size - 1 {
+                    plyrs.retain(|p| !id_buffer.contains(p));
+                    id_buffer.push(first);
+                    digest.paired.push(id_buffer);
+                    continue 'outer;
                 }
             }
         }
-        if index_buffer.len() == match_size {
-            let mut pairing: Vec<PlayerId> = Vec::with_capacity(match_size);
-            for (count, i) in index_buffer.iter().enumerate() {
-                let id = plyrs.remove(i - count);
-                pairing.push(id);
-            }
-            digest.paired.push(pairing);
-        } else {
-            digest.rejected.push(plyrs.pop().unwrap());
-        }
+
+        digest.rejected.push(first);
     }
-    digest.rejected.extend_from_slice(&plyrs);
+    digest.rejected.extend(plyrs);
     digest
 }
 
 /// Checks to see if a player can be apart of a potential pairing
-fn valid_pairing(
+fn valid_pairing<'a>(
     past_opponents: &HashMap<PlayerId, HashSet<PlayerId>>,
-    known: &[PlayerId],
+    known: impl Iterator<Item = &'a PlayerId>,
     new: &PlayerId,
     repair_tol: u64,
 ) -> bool {
-    if let Some(opps) = past_opponents.get(new) {
-        known.iter().filter(|p| opps.contains(p)).count() as u64 <= repair_tol
-    } else {
-        true
-    }
+    past_opponents.get(new).map_or(true, |opps| {
+        known.filter(|p| opps.contains(p)).count() as u64 <= repair_tol
+    })
 }
