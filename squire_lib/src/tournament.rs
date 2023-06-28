a607569f8d8ddc0a8d594b93a00d8c2fa0429d5d
zbrachinara <zbrachinara@gmail.com>
2023-06-28 12:41:07 -0400

    correct expected results and cases

--- squire_lib/src/tournament.rs
+++ squire_lib/src/tournament.rs
@@ -1278,7 +1278,6 @@ mod tests {
         let mut tourn =
             Tournament::from_preset("Test".into(), TournamentPreset::Swiss, "Test".into());
         assert_eq!(tourn.pairing_sys.common.match_size, 2);
-        assert!(tourn.start().is_ok());
 
         fn make_player(tourn: &mut Tournament) -> PlayerId {
             tourn
@@ -1296,23 +1295,34 @@ mod tests {
         let players_correct = std::iter::repeat_with(|| make_player(&mut tourn))
             .take(2)
             .collect_vec();
-        assert_eq!(
-            tourn.create_round(default_salt, players_too_large),
-            Err(TournamentError::InvalidMatchSize)
-        );
-        assert_eq!(
-            tourn.create_round(default_salt, players_too_small),
-            Err(TournamentError::InvalidMatchSize)
-        );
-        assert_eq!(
-            tourn.create_round(default_salt, players_none),
-            Err(TournamentError::InvalidMatchSize)
-        );
-        assert!(tourn.create_round(default_salt, players_correct).is_ok());
-
         let repeated_players = std::iter::repeat(make_player(&mut tourn))
             .take(2)
             .collect_vec();
+
+        assert!(tourn.start().is_ok());
+
+        assert_eq!(
+            tourn.create_round(default_salt, players_too_large),
+            Err(TournamentError::IncorrectMatchSize)
+        );
+        assert_eq!(
+            tourn.create_round(default_salt, players_too_small),
+            Err(TournamentError::IncorrectMatchSize)
+        );
+        assert_eq!(
+            tourn.create_round(default_salt, players_none),
+            Err(TournamentError::IncorrectMatchSize)
+        );
+        assert!(tourn.create_round(default_salt, players_correct).is_ok());
+
+        let unregistered_players = std::iter::repeat_with(|| PlayerId::new(Uuid::new_v4()))
+            .take(2)
+            .collect_vec();
+        assert_eq!(
+            tourn.create_round(default_salt, unregistered_players),
+            Err(TournamentError::PlayerNotFound)
+        );
+
         assert_eq!(
             tourn.create_round(default_salt, repeated_players),
             Err(TournamentError::RepeatedPlayerInMatch)
