64fad868ffcb82800f022bd605f23845e0b7a566
zbrachinara <zbrachinara@gmail.com>
2023-07-03 16:16:27 -0400

    use settings in db-tests

--- squire_core/src/tests/state.rs
+++ squire_core/src/tests/state.rs
@@ -1,5 +1,4 @@
 #![cfg(feature = "db-tests")]
-// TODO force tests to run independently (crate `serial_test`?)
 
 use async_session::chrono::Utc;
 use squire_sdk::{
@@ -7,7 +6,7 @@ use squire_sdk::{
     tournaments::{TournOp, TournamentManager},
 };
 
-use crate::state::AppState;
+use crate::state::{AppSettings, AppState};
 
 async fn clear_database() {
     AppState::new().await.get_db().drop(None).await;
@@ -18,7 +17,10 @@ async fn insert_fetch_tourn() {
     clear_database().await;
 
     let manager = TournamentManager::new(squire_tests::spoof_account(), squire_tests::get_seed());
-    let state = AppState::new().await;
+    let state = AppState::new_with_settings(
+        AppSettings::default().database_name("SquireTesting_insert_fetch_tourn"),
+    )
+    .await;
 
     state.persist_tourn(&manager).await;
     let retrieved_tourn = state
@@ -35,7 +37,10 @@ async fn check_already_persisted() {
 
     let mut manager =
         TournamentManager::new(squire_tests::spoof_account(), squire_tests::get_seed());
-    let state = AppState::new().await;
+    let state = AppState::new_with_settings(
+        AppSettings::default().database_name("SquireTesting_check_already_persisted"),
+    )
+    .await;
 
     assert!(!state.persist_tourn(&manager).await);
     assert!(state.persist_tourn(&manager).await);
