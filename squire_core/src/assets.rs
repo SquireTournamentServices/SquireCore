beb926ab833d5872cbc34cbe0ddf8ddad376761b
TylerBloom <tylerbloom2222@gmail.com>
2023-07-20 12:16:21 -0700

    Cleaned up unused files in SC

--- squire_core/src/assets.rs
+++ squire_core/src/assets.rs
@@ -1,47 +1,15 @@
-use std::env;
-
 use axum::response::{Html, Response};
-use http::header;
+use http::{header, HeaderMap, HeaderValue, StatusCode};
 use hyper::{body::Bytes, Body};
 
-#[cfg(debug_assertions)]
-pub async fn landing() -> Html<String> {
-    let wd = env::var("CARGO_MANIFEST_DIR").unwrap();
-    Html(
-        tokio::fs::read_to_string(format!("{wd}/../assets/index.html"))
-            .await
-            .unwrap(),
-    )
-}
-
-#[cfg(debug_assertions)]
-pub async fn get_wasm() -> Response<Body> {
-    let wd = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let wasm = tokio::fs::read(format!("{wd}/../assets/squire_web_bg.wasm"))
-        .await
-        .unwrap();
-    let bytes = Bytes::copy_from_slice(&wasm);
-    let body: Body = bytes.into();
-
-    Response::builder()
-        .header(header::CONTENT_TYPE, "application/wasm")
-        .body(body)
-        .unwrap()
-}
-
-#[cfg(not(debug_assertions))]
 const INDEX_HTML: &str = include_str!("../../assets/index.html");
-#[cfg(not(debug_assertions))]
 const APP_WASM: &[u8] = include_bytes!("../../assets/squire_web_bg.wasm");
-//#[cfg(not(debug_assertions))]
 const APP_JS: &str = include_str!("../../assets/squire_web.js");
 
-#[cfg(not(debug_assertions))]
 pub async fn landing() -> Html<&'static str> {
     Html(INDEX_HTML)
 }
 
-#[cfg(not(debug_assertions))]
 pub async fn get_wasm() -> Response<Body> {
     let bytes = Bytes::copy_from_slice(APP_WASM);
     let body: Body = bytes.into();
@@ -52,23 +20,11 @@ pub async fn get_wasm() -> Response<Body> {
         .unwrap()
 }
 
-#[cfg(not(debug_assertions))]
-pub async fn get_js() -> Response<String> {
-    Response::builder()
-        .header(header::CONTENT_TYPE, "application/javascript;charset=utf-8")
-        .body(APP_JS.to_string())
-        .unwrap()
-}
-
-#[cfg(debug_assertions)]
-pub async fn get_js() -> Response<String> {
-    let wd = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let js = tokio::fs::read_to_string(format!("{wd}/../assets/squire_web.js"))
-        .await
-        .unwrap();
-
-    Response::builder()
-        .header(header::CONTENT_TYPE, "application/javascript;charset=utf-8")
-        .body(js)
-        .unwrap()
+pub async fn get_js() -> (StatusCode, HeaderMap, &'static str) {
+    let mut headers = HeaderMap::with_capacity(1);
+    headers.insert(
+        header::CONTENT_TYPE,
+        HeaderValue::from_static("application/javascript;charset=utf-8"),
+    );
+    (StatusCode::OK, headers, APP_JS)
 }
