2843569f7705fd680dd2168347f23d395a9a7766
craigslisttrashman <fintanneff@gmail.com>
2023-07-29 12:23:52 -0400

    Starting work on refactoring viewer components.
    Starting work on refactoring viewer components.

--- squire_web/src/tournament/mod.rs
+++ squire_web/src/tournament/mod.rs
@@ -1,3 +1,6 @@
+use squire_sdk::tournaments::TournamentId;
+// use squire_sdk::tournaments::TournamentManager;
+// use wasm_bindgen_futures::spawn_local;
 use yew::{Component, Context};
 
 use crate::ON_UPDATE;
@@ -23,3 +26,67 @@ where
         to_return
     })
 }
+
+
+/*
+pub struct TournViewerComponentWrapper<T> {
+    comp: T,
+}
+
+enum WrapperMessage<T>
+where T: TournViewerComponent
+{
+    Interaction(T::InteractionMessage),
+    ReQuery,
+    QueryData(T::QueryMessage),
+    RemoteUpdate(TournamentId),
+}
+
+impl<T> Component for TournViewerComponentWrapper<T>
+where T: TournViewerComponent + 'static
+{
+    type Message = WrapperMessage<T>;
+    type Properties;
+
+    fn create(ctx: &yew::Context<Self>) -> Self {
+        todo!()
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            WrapperMessage::Interaction(msg) => self.comp.interaction(msg),
+            WrapperMessage::ReQuery => self.comp.query(ctx),
+            WrapperMessage::QueryData(data) => self.comp.load_queried_data(data),
+            WrapperMessage::RemoteUpdate(t_id) if self.t_id == t_id => self.comp.query(ctx),
+            _ => false,
+        }
+        todo!()
+    }
+
+    fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
+        todo!()
+    }
+
+    fn changed(&mut self, ctx: &Context<Self>, _old_props: &Self::Properties) -> bool {
+        true
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {}
+
+    fn prepare_state(&self) -> Option<String> {
+        None
+    }
+
+    fn destroy(&mut self, ctx: &Context<Self>) {}
+}
+pub trait TournViewerComponent {
+    type InteractionMessage;
+    type QueryMessage;
+
+    fn load_queried_data(&mut self, msg: Self::QueryMessage) -> bool;
+    
+    fn interaction(&mut self, msg: Self::InteractionMessage) -> bool;
+
+    fn query(&mut self, ctx: &Context<TournViewerComponentWrapper<Self>>);
+}
+*/
--- squire_web/src/utils/mod.rs
+++ squire_web/src/utils/mod.rs
@@ -5,6 +5,7 @@ pub mod requests;
 pub use input::*;
 pub use popout::*;
 pub use requests::*;
+// use yew::{Html, html};
 
 /// A wrapper around web_sys console log_1
 #[allow(unused)]
@@ -20,5 +21,34 @@ where
     let digest = *storage != data;
     *storage = data;
     digest
+}
+*/
+
+/*
+pub fn tabularize<H, I, T, M>(header: H, iter: I, map: M) -> Html 
+where
+H: Iterator<Item=String>, 
+I: Iterator<Item=T>, 
+M: FnMut(T) -> IntoIterator<Item=String, IntoIter=String>
+{
+    let list = iter
+        .map(map)
+        .map(|inner| html!{ <tr> { inner.map(|item| html! { <td> { item } </td> }) } </tr> })
+        .collect::<Html>();
+    let header = html! {
+        <thead>
+            <tr>
+                { header.map(|h| html! { <th> { h } </th> }).collect::<Html>() }
+            </tr>
+        </thead>
+        };
+    html! {
+        <>
+        <table class="table">
+            { header }
+            <tbody> { list } </tbody>
+        </table>
+        </>
+    }
 }
 */
