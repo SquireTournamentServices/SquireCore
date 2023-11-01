3b58ca8e052a6b11f644553e9ff447214de3ec72
craigslisttrashman <fintanneff@gmail.com>
2023-11-01 11:00:26 -0400

    Progress on the registration page.
    Progress on the registration page.

--- squire_web/src/account/login.rs
+++ squire_web/src/account/login.rs
@@ -1,10 +1,62 @@
-use yew::{functional::*, prelude::*};
+use std::borrow::Cow;
 
-#[function_component(Login)]
-pub fn login() -> Html {
-    html! {
-        <div>
-            <p>{ "Log into an existing account" }</p>
-        </div>
+use yew::prelude::*;
+
+use crate::{utils::TextInput, CLIENT};
+
+pub enum LoginMessage {
+    NameInput(String),
+    PasswordInput(String),
+    SubmitLogin,
+}
+
+pub struct Login {
+    input: (Option<String>, Option<String>),
+}
+
+impl Login {
+    fn _get_logform(&self) -> Result<String, String> {
+        todo!()
+    }
+}
+
+impl Component for Login {
+    type Message = LoginMessage;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            input: (None, None),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            LoginMessage::NameInput(s) => self.input.0 = Some(s),
+            LoginMessage::PasswordInput(s) => self.input.1 = Some(s),
+            LoginMessage::SubmitLogin => {
+                let _client = CLIENT.get().unwrap();
+                todo!();
+            },
+        }
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let submit_callback = ctx.link().callback(|_| LoginMessage::SubmitLogin);
+        let name_callback = ctx.link().callback(LoginMessage::NameInput);
+        let password_callback = ctx.link().callback(LoginMessage::PasswordInput);
+        let form = html! {
+            <div>
+                <TextInput label = {Cow::from("Username")} process = { name_callback } />
+                <TextInput label = {Cow::from("Password")} process = { password_callback } />
+            </div>
+        };
+        html! {
+            <div>
+                { form }
+                <button onclick={submit_callback}>{ "Log in" }</button>
+            </div>
+        }
     }
 }
--- squire_web/src/account/register.rs
+++ squire_web/src/account/register.rs
@@ -1,20 +1,39 @@
 use std::borrow::Cow;
 
+use squire_sdk::api::RegForm;
 use yew::prelude::*;
-use yew_router::prelude::*;
+// use yew_router::prelude::*;
 
-use crate::{utils::TextInput, Route};
+use crate::{utils::TextInput, CLIENT};
 
 pub enum RegisterMessage {
     NameInput(String),
     DisplayInput(String),
-    EmailInput(String),
     PasswordInput(String),
     RePasswordInput(String),
+    SubmitRegister,
+    RegisterResult(Result<bool, reqwest::Error>),
 }
 
 pub struct Register {
-    input: (Option<String>, Option<String>, Option<String>, Option<String>, Option<String>),
+    input: (Option<String>, Option<String>, Option<String>, Option<String>),
+}
+
+impl Register {
+    fn get_regform(&self) -> Result<RegForm, String> {
+        let name = self.input.0.clone().unwrap_or_else( || { "".to_owned() } );
+        if name.is_empty() { return Err("You need to enter a username".to_owned()); }
+        let display = self.input.1.clone().unwrap_or_else( || { "".to_owned() } );
+        if display.is_empty() { return Err("You need to enter a display name.".to_owned()); }
+        let password = self.input.2.clone().unwrap_or_else( || { "".to_owned() } );
+        if password.is_empty() { return Err("You need to enter a password".to_owned()); }
+        let repassword = self.input.3.clone().unwrap_or_else( || { "".to_owned() } );
+        if repassword.is_empty() { return Err("You need to re-enter your password".to_owned()); }
+        if password != repassword { return Err("Your entered passwords need to match.".to_owned()); }
+        Ok (
+            RegForm { username: name, display_name: display, password: password}
+        )
+    }
 }
 
 impl Component for Register {
@@ -23,44 +42,45 @@ impl Component for Register {
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            input: (None, None, None, None, None),
+            input: (None, None, None, None),
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             RegisterMessage::NameInput(s) => self.input.0 = Some(s),
             RegisterMessage::DisplayInput(s) => self.input.1 = Some(s),
-            RegisterMessage::EmailInput(s) => self.input.2 = Some(s),
-            RegisterMessage::PasswordInput(s) => self.input.3 = Some(s),
-            RegisterMessage::RePasswordInput(s) => self.input.4 = Some(s),
+            RegisterMessage::PasswordInput(s) => self.input.2 = Some(s),
+            RegisterMessage::RePasswordInput(s) => self.input.3 = Some(s),
+            RegisterMessage::SubmitRegister => {
+                let client = CLIENT.get().unwrap();
+                ctx.link().send_future(
+                    client.register(self.get_regform().unwrap()).output()
+                );
+                return false;
+            },
+            RegisterMessage::RegisterResult(result) => {
+                todo!("{result:?}")
+            }
         }
         true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let do_submit = 
-            self.input.0.is_some() && 
-            self.input.1.is_some() &&
-            self.input.2.is_some() &&
-            self.input.3.is_some() &&
-            self.input.4.is_some();
-        let navigator = ctx.link().navigator().unwrap();
-        let submit_callback = Callback::from(move |_| {
-            if do_submit {
-                navigator.push(&Route::Create);
-            }
-        });
+        // let navigator = ctx.link().navigator().unwrap();
+        /*
+        let submit_callback = move |_| {
+            RegisterMessage::SubmitRegister);
+        */
+        let submit_callback = ctx.link().callback(|_| RegisterMessage::SubmitRegister);
         let name_callback = ctx.link().callback(RegisterMessage::NameInput);
         let display_callback = ctx.link().callback(RegisterMessage::DisplayInput);
-        let email_callback = ctx.link().callback(RegisterMessage::EmailInput);
         let password_callback = ctx.link().callback(RegisterMessage::PasswordInput);
         let repassword_callback = ctx.link().callback(RegisterMessage::RePasswordInput);
         let form = html! {
             <div>
-                <TextInput label = {Cow::from("Your name")} process = { name_callback } />
+                <TextInput label = {Cow::from("Username")} process = { name_callback } />
                 <TextInput label = {Cow::from("Display name")} process = { display_callback } />
-                <TextInput label = {Cow::from("Email Address")} process = { email_callback } />
                 <TextInput label = {Cow::from("Password")} process = { password_callback } />
                 <TextInput label = {Cow::from("Re-Type Password")} process = { repassword_callback } />
             </div>
@@ -71,5 +91,11 @@ impl Component for Register {
                 <button onclick={submit_callback}>{ "Register" }</button>
             </div>
         }
+    }
+}
+
+impl From<Result<bool, reqwest::Error>> for RegisterMessage {
+    fn from(value: Result<bool, reqwest::Error>) -> Self {
+        Self::RegisterResult(value)
     }
 }
